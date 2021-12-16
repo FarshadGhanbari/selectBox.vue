a8b3683216929d66b888a7a67a86434ebc745a79
Farshad Ghanbari <eng.ghanbari2025@gmail.com>
2021-12-16 23:37:34 +0330

    Create selectBox.vue

---
+++ selectBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <select :value="getValue" @change="$emit('change', $event.target.value)" class="transition-all cursor-pointer bg-white dark:bg-neutral-700 border border-gray-200 focus:border-gray-400 dark:border-neutral-800 dark:focus:border-neutral-900 dark:text-neutral-300 focus:ring-0 focus:ring-transparent p-3 ring-0 text-sm text-gray-600 dark:text-gray-400 rounded-md">
+    <slot/>
+  </select>
+</template>
+<script>
+export default {
+  model: {
+    prop: "modelValue",
+    event: "change"
+  },
+  props: {
+    value: {
+      type: String
+    },
+    modelValue: {
+      default: ""
+    }
+  },
+  computed: {
+    getValue() {
+      return this.modelValue
+    }
+  }
+}
+</script>
